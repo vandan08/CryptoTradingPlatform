c2e94a135efd3fb468b2661ed393de574f825508
admin <vandanbsheth9@gmail.com>
2025-06-25 22:08:09 +0530

    Intial Commit

---
+++ src/main/java/com/vs/TradingApplication.java
@@ -0,0 +1,14 @@
+package com.vs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingApplication.class, args);
+		System.out.println("Heelo");
+	}
+
+}
