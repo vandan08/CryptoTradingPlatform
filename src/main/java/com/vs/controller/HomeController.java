4f4682147c90daa5c69be1144db97233bb8718a0
Vandan Sheth <127378451+vandan08@users.noreply.github.com>
2025-07-04 21:22:26 +0530

    added response and removed application.properties

--- src/main/java/com/vs/controller/HomeController.java
+++ src/main/java/com/vs/controller/HomeController.java
@@ -1,13 +1,27 @@
 package com.vs.controller;
 
+
+import com.vs.response.ApiResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
-public class HomeController {
+public class   HomeController {
 
 	@GetMapping("")
-	public String home() {
-		return "Welocme to Crypto Trading Platform";
+	public ResponseEntity<ApiResponse> homeController(){
+
+
+		ApiResponse res=new ApiResponse(
+				"welcome to crypto treading platform working fine",
+				true
+		);
+		return new ResponseEntity<ApiResponse>(res,HttpStatus.ACCEPTED);
 	}
+
 }
