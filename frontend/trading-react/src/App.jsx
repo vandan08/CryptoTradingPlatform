fcd514b386b9f1941d30b77aa6d6469e72e67f08
Vandan Sheth <127378451+vandan08@users.noreply.github.com>
2025-07-10 21:12:59 +0530

    added navbar and sidebar to project and related configurations

--- frontend/trading-react/src/App.jsx
+++ frontend/trading-react/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button'
+import Navbar from "./page/Navbar/Navbar"
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button>Vandan Sheth</Button>
+      <Navbar></Navbar>
     </>
   )
 }
